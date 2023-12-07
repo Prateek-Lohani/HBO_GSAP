ea6be21c278af4b714088773520f25b7e3840708
Prateek Lohani <67368888+Prateek-Lohani@users.noreply.github.com>
2023-12-08 00:43:02 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,24 @@
+var tl=gsap.timeline();
+
+tl.from('h3',{
+  y:-100,
+  duration:.6,
+  color:'red',
+  stagger:0.2
+})
+
+tl.from('img',{
+  opacity:0,
+  x:-100,
+  duration:0.6,
+  scale:0
+})
+
+tl.from('.name h1',{
+  x:100,
+  duration:.8,
+  stagger:0.5,
+  opacity:0,
+  
+})
+
